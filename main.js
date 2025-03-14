8c523f5a7136eb5ca269fbdad4c157fe5ca0cb1c
mohamed <me.mo.75.20.09.biomy@gmail.com>
2025-03-14 16:04:23 +0200

    Large screen page has been created

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let btn = document.querySelector(".share-btn")
 let info = document.querySelector(".info")
 let links = document.querySelector(".links")
 let activeState = true
+let icon = document.querySelector(".share-btn i")
 
 console.log(btn)
 console.log(links)
@@ -10,15 +11,32 @@ console.log(links)
 links.remove()
 
 btn.addEventListener("click", () => {
-  if (activeState === true) {
-    info.remove()
-    social.prepend(links)
-    social.classList.add("active")
-    activeState = false
-  } else if (activeState === false) {
-    links.remove()
-    social.prepend(info)
-    social.classList.remove("active")
-    activeState = true
+  if (window.innerWidth < 900) {
+    if (activeState === true) {
+      info.remove()
+      social.prepend(links)
+      social.classList.add("active")
+      activeState = false
+    } else if (activeState === false) {
+      links.remove()
+      social.prepend(info)
+      social.classList.remove("active")
+      activeState = true
+    } 
+  } else {
+    if (activeState === true) {
+      btn.style.backgroundColor = "var(--desaturated-dark-blue)"
+      icon.style.color = "var(--light-grayish-blue)"
+      social.prepend(links)
+      activeState = false
+    } else if (activeState === false) {
+      links.remove()
+      btn.style.backgroundColor = "var(--light-grayish-blue)"
+      icon.style.color = "var(--very-dark-grayish-blue)"
+      activeState = true
+    }
   }
 })
+
+// background-color: var(--desaturated-dark-blue);
+// color: var(--very-dark-grayish-blue);
